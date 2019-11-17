1920312e873001810b58fe766b2c84214242dc41
Jeremy Kirscht <jeremykirscht@gmail.com>
2019-11-17 12:22:35 -0600

    Create raspi-3-init-setup.sh
    Script to setup Raspberry Pi 3 after fresh reinstall of Raspbian

---
+++ raspi-3-init-setup.sh
@@ -0,0 +1,20 @@
+# Update 
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+# Install dev stuff
+sudo apt-get install build-essential libncurses-dev bison flex libssl-dev libelf-dev
+
+# Install Go
+sudo apt-get install golang
+
+# Install Docker
+curl -sSL get.docker.com | sh && \
+sudo usermod pi -aG docker && \
+newgrp docker
+
+# Disable Swapfile
+sudo dphys-swapfile swapoff 
+sudo dphys-swapfile uninstall
+sudo update-rc.d dphys-swapfile remove
+sudo swapon --summary
